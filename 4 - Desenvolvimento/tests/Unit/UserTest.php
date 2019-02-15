2f9a52fbfff375862b8ed1a5e906982141c42745
Diego Menegazzi <omenegazzi@gmail.com>
2019-02-15 17:38:31 -0200

    Implementado testes TDD do Usuário.

---
+++ 4 - Desenvolvimento/tests/Unit/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_create_user(){
+        User::create([
+            'name' => 'Admin user',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt(123456),
+            'enrolment_number' => '1200660'
+        ]);
+
+        $this->assertDatabaseHas('users', ['name' => 'Admin User']);
+
+
+    }
+}
