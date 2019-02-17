c7c9ee8477514940b051f80f23fde4984f1ed152
Diego Menegazzi <omenegazzi@gmail.com>
2019-02-17 15:15:54 -0300

    Primeiro commit

--- 4 - Desenvolvimento/tests/Unit/UserTest.php
+++ 4 - Desenvolvimento/tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,15 +13,25 @@ class UserTest extends TestCase
     use DatabaseTransactions;
 
     public function test_create_user(){
-        User::create([
+        $user = User::create([
             'name' => 'Admin user',
             'email' => 'admin@admin.com',
             'password' => bcrypt(123456),
             'enrolment_number' => '1200660'
         ]);
 
-        $this->assertDatabaseHas('users', ['name' => 'Admin User']);
+        $userProfile = UserProfile::create([
+            'user_id' => $user->id,
+            'gender' => 'M',
+            'cpf' => '06744148977',
+            'zip' => 'home_phone',
+        ])
 
+
+        $this->assertDatabaseHas('users', ['name' => 'Admin User']);
+        $this->assertDatabaseHas('user_profile', [
+            'user_id' => $user->id,
+            'address'])
 
     }
 }
